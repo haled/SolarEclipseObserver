46b101614f44b0c49ded515aa877821446fb63ff
haled <darren.e.hale@gmail.com>
2017-05-21 21:44:34 -0500

    Adding my playground code.

---
+++ station/camera_playground.py
@@ -0,0 +1,51 @@
+from picamera import PiCamera
+from time import sleep
+
+
+camera = PiCamera()
+
+
+##camera.rotation = 180
+##camera.start_preview()
+##sleep(10)
+##camera.stop_preview()
+
+
+# rotation
+# camera.rotation = 180 (flip image upside down)
+
+
+# still pictures
+# should sleep for at least 2 seconds so light levels can be set
+# camera.capture('name_of_image.jpg')
+# the line above goes between the start/stop preview lines
+
+
+# camera.start_preview()
+# for i in range(5):
+#     sleep(5)
+#     camera.capture('/home/darren/Desktop/image%s.jpg' % i)
+# camera.stop_preview()
+camera.rotation = 180
+camera.start_preview()
+camera.annotate_text = "Test Image"
+sleep(5)
+camera.capture('/home/darren/Desktop/test_image.jpg')
+camera.stop_preview
+
+
+# VIDEO
+# camera.start_preview()
+# camera.start_recording('name_of_video_file')
+# sleep(10)
+# camera.stop_recording()
+# camera.stop_preview()
+
+
+# PROPERTIES
+# camera.resolution = (x, y)
+# camera.framerate = z
+# camera.annotate_text = "text here"
+# camera.annotation_text_size = 6 to 160 (default 32)
+# camera.brightness = p
+# camera.contrast = q
