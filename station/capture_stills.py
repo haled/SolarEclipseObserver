75b86627403f7d82928bb25d93c6da9f84dcb548
haled <darren.e.hale@gmail.com>
2017-07-04 15:51:57 -0500

    Added the ability to take pictures over a period of time. Also added a date/time stamp to the image names.

--- station/capture_stills.py
+++ station/capture_stills.py
@@ -1,12 +1,28 @@
 from picamera import PiCamera
 from time import sleep
+import datetime
+import sys
 
 camera = PiCamera()
 
 camera.rotation = 180
 camera.resolution = (360,360)
 
-for i in range(5):
-    camera.annotate_text = 'Test Image %s' % i
-    sleep(5)
-    camera.capture('./pics/test_image_%s.jpg' % i)
+startTime = datetime.datetime.now()
+timeDiff = 0
+
+#for i in range(5):
+while (timeDiff < 600):
+    #camera.annotate_text = 'Test Image %s' % i
+    sleep(60)
+    currentDateTime = datetime.datetime.now()
+    currentDateTimeString = currentDateTime.strftime("%Y%m%d%H%M%S")
+    camera.capture('./pics/test_image_%s.jpg' % currentDateTimeString)
+    timeDiff = (currentDateTime - startTime).total_seconds()
+    print(timeDiff)
+
+endTime = datetime.datetime.now()
+
+totalTime = endTime - startTime
+
+print(totalTime)
